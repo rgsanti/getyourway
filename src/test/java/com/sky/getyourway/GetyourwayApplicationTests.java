f03583614039eab5d3bf24f5c84352276274d19d
Raphael Santiago <raphaelgabriel.santiago@sky.uk>
2022-07-22 15:45:45 +0100

    Initialised Spring Boot project

---
+++ src/test/java/com/sky/getyourway/GetyourwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.getyourway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetyourwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
