086402c93cc513ab2d91a95fc1221d9313ab6156
Raphael Santiago <raphaelgabriel.santiago@sky.uk>
2022-07-22 16:42:52 +0100

    Added User entity class

--- src/main/java/com/sky/getyourway/GetyourwayApplication.java
+++ src/main/java/com/sky/getyourway/GetyourwayApplication.java
@@ -1,5 +1,6 @@
 package com.sky.getyourway;
 
+import com.sky.getyourway.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/sky/getyourway/entities/User.java
+++ src/main/java/com/sky/getyourway/entities/User.java
@@ -1,4 +1,45 @@
 package com.sky.getyourway.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(min = 1, max = 30)
+    @NotNull
+    private String firstName;
+
+    @Size(min = 1, max = 30)
+    @NotNull
+    private String lastName;
+
+    @Email
+    @NotNull
+    private String email;
+
+    // TODO: Validation for password format?
+    @NotNull
+    private String password;
+
+    @Size(min = 11, max = 11)
+    @NotNull
+    private String phoneNumber;
+
+    @Size(min = 5, max = 8)
+    private String postcode;
 }
