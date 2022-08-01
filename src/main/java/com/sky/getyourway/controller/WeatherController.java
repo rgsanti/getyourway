6174574c04d5fdc73deeb56703e2845f505f1a98
Ibraheem Al-Adhami <ialadh200@caledonian.ac.uk>
2022-08-01 11:28:10 +0100

    Integrate weather config and the uri building

--- src/main/java/com/sky/getyourway/controller/WeatherController.java
+++ src/main/java/com/sky/getyourway/controller/WeatherController.java
@@ -3,19 +3,30 @@ package com.sky.getyourway.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sky.getyourway.entities.Weather;
 import org.apache.tomcat.util.json.ParseException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.json.JSONObject;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 
 @RestController
-@RequestMapping("api/weather")
+@RequestMapping("/api/weather")
 public class WeatherController {
+
+    @Value("${client.weather.api.token}")
+    private String token;
+    @Value("${client.weather.api.url}")
+    private String url;
+
     @GetMapping(value="/weather")
     private String getWeather() throws JsonProcessingException, ParseException {
-        String uri = "https://api.openweathermap.org/data/2.5/weather?q=London&appid={apikey}}";        RestTemplate rt = new RestTemplate();
+        String uri = buildUri("london").toString();
+        RestTemplate rt = new RestTemplate();
         String jsonString = rt.getForObject(uri, String.class);
         JSONObject jo = new JSONObject(jsonString);
 
@@ -26,12 +37,17 @@ public class WeatherController {
         String iconLink = "https://openweathermap.org/img/wn/"+icon+"@2x.png";
 
         Weather w1 = new Weather(description, iconLink);
-        System.out.println(jo);
-        System.out.println(ja);
-        System.out.println(description);
-        System.out.println(icon);
 
         return w1.toString();
+    }
+
+    public URI buildUri(String location)
+    {
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(url);
+        uriBuilder.queryParam("q", location);
+        uriBuilder.queryParam("appid", token);
+
+        return uriBuilder.build().toUri();
     }
 }
 
